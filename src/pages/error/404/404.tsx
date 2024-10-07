67e76677e199cb4497f6b1e4537b7a62ef9d54e5
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-10-08 00:17:49 +0300

    Добавить страницу ошибки 404

---
+++ src/pages/error/404/404.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
+
+function Error404(): JSX.Element {
+  return (
+    <div className={"page " + styles.errorPage}>
+      <div className={styles.errorBlock}>
+        <h1 className={styles.errorCode}>
+          404
+        </h1>
+        <h2 className={styles.errorMessage}>
+          Page not found
+        </h2>
+      </div>
+      <Link to="/" className={styles.linkMain}>
+        Main page
+      </Link>
+    </div>
+  )
+}
+
+export default Error404;
