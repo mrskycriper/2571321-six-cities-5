816c38ce47fc5f493c400acab185d82a5605b77a
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-10-21 18:43:02 +0300

    Определить типы для предложений

---
+++ src/entities/offer.ts
@@ -0,0 +1,58 @@
+import { UserEntity } from "./user";
+
+export type OfferImage = {
+  id: number,
+  src: string,
+  alt: string,
+  coverImage?: boolean
+}
+
+export type OfferMark = 'Premium';
+
+export type OfferRating = {
+  numericValue: number,
+  starValue: number
+};
+
+export type OfferFeatures = {
+  placeType: 'Apartment' | 'Room',
+  bedroomCount?: number,
+  maxAdultOccupancy: number
+}
+
+export type OfferPrice = {
+  value: number,
+  period: 'night'
+}
+
+export type OfferInsideItem = {
+  id: number,
+  text: string
+}
+
+export type OfferDescriptionItem = {
+  id: number,
+  text: string
+}
+
+export type OfferReview = {
+  id: string,
+  user: UserEntity,
+  stars: 1 | 2 | 3 | 4 | 5,
+  text: string,
+  datetime: string
+}
+
+export type OfferEntity = {
+  id: string,
+  images: OfferImage[],
+  mark?: OfferMark,
+  name: string,
+  rating: OfferRating,
+  features: OfferFeatures,
+  price: OfferPrice,
+  insideList: OfferInsideItem[],
+  host: UserEntity,
+  description: OfferDescriptionItem[],
+  reviews: OfferReview[]
+};
