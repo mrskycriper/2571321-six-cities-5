8c5e2373306f2913e77879353c0db816b6579a9c
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-10-22 15:24:22 +0300

    Выделить рейтинг в отдельный компонент

---
+++ src/components/rating/rating.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames';
+
+type RatingProps = {
+  numericValue?: number;
+  starValue: number;
+  containerClassName: string;
+  starsClassName: string;
+  valueClassName?: string
+};
+
+function Rating({
+  numericValue,
+  starValue,
+  containerClassName,
+  starsClassName,
+  valueClassName = 'offer__rating-value',
+}: RatingProps): JSX.Element {
+  return (
+    <div className={classNames(containerClassName, 'rating')}>
+      <div className={classNames(starsClassName, 'rating__stars')}>
+        <span style={{ width: `${20 * starValue}%` }}></span>
+        <span className="visually-hidden">Rating</span>
+      </div>
+      {numericValue !== undefined ? (
+        <span className={classNames(valueClassName, 'rating__value')}>{numericValue}</span>
+      ) : null}
+    </div>
+  );
+};
+
+export default Rating;
