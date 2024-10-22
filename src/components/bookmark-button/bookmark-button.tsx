8e31bb1360cf040d07bc690e4d4d128f44449671
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-10-22 15:24:46 +0300

    Выделить кнопку закладки в отдельный компонент

---
+++ src/components/bookmark-button/bookmark-button.tsx
@@ -0,0 +1,18 @@
+type BookmarkButtonProps = {
+  marked: boolean;
+};
+
+function BookmarkButton({ marked }: BookmarkButtonProps): JSX.Element {
+  return (
+    <>
+      <button className={`place-card__bookmark-button${marked ? ' place-card__bookmark-button--active' : ''} button`} type="button">
+        <svg className="place-card__bookmark-icon" width="18" height="19">
+          <use xlinkHref="#icon-bookmark"></use>
+        </svg>
+        <span className="visually-hidden">{marked ? 'To bookmarks' : 'In bookmarks'}</span>
+      </button>
+    </>
+  );
+};
+
+export default BookmarkButton;
