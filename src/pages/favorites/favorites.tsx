6ea57fd05ea358a08856598c26a92464a3bc8af7
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-11-04 22:29:46 +0300

    Выделить заголовок в компонент

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header/header';
 import { OffersListFavorites } from '@/components/offers-lists/offers-lists';
 import { OfferEntity } from '@/types/offer/offer';
 
@@ -8,44 +9,7 @@ type FavoritesProps = {
 function Favorites({ offers }: FavoritesProps): JSX.Element {
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header isLoggedIn/>
       <OffersListFavorites offers={offers}/>
       <footer className="footer container">
         <a className="footer__logo-link" href="main.html">
