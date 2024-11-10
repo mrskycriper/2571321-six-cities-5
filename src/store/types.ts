db3bf4b5ce5ff9efb40d02869845049cadef3a69
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-11-10 23:21:36 +0300

    Add typed store hooks

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,7 +1,10 @@
 import { City } from '@/types/city/city';
 import { OfferEntity } from '@/types/offer/offer';
+import { store } from '.';
 
 export type State = {
   city: City;
   offers: OfferEntity[];
 };
+
+export type AppDispatch = typeof store.dispatch
