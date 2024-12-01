4ab2d49990fe28129fb0b226b6ee6c01418d5064
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-12-02 00:15:49 +0300

    Implement login page logic

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import Header from '@/components/header';
+import { APP_ROUTES } from '@/constants/routes';
+import { login } from '@/store/actions';
+import { useAppSelector, useAppDispatch } from '@/store/hooks';
+import { validatePassword } from '@/utils/user';
 
 function Login(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { authorizationStatus } = useAppSelector((state) => state.userReducer);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  useEffect(() => {
+    if (validatePassword(password)) {
+      setSubmitDisabled(false);
+    } else {
+      setSubmitDisabled(true);
+    }
+  }, [password]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(login({ email: email, password: password }));
+  };
+
+  useEffect(() => {
+    if (authorizationStatus) {
+      navigate(APP_ROUTES.MAIN);
+    }
+  }, [navigate, authorizationStatus]);
+
   return (
     <div className="page page--gray page--login">
       <Header isLoginPage />
@@ -8,7 +48,12 @@ function Login(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form"
+              action="#"
+              method="post"
+              onSubmit={handleSubmit}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
@@ -17,6 +62,7 @@ function Login(): JSX.Element {
                   name="email"
                   placeholder="Email"
                   required
+                  onChange={handleEmailChange}
                 />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
@@ -27,11 +73,13 @@ function Login(): JSX.Element {
                   name="password"
                   placeholder="Password"
                   required
+                  onChange={handlePasswordChange}
                 />
               </div>
               <button
                 className="login__submit form__submit button"
                 type="submit"
+                disabled={submitDisabled}
               >
                 Sign in
               </button>
