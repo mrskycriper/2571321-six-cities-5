74722cdab8e8e4ec3c5daec259c36701b944060c
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-12-03 00:24:45 +0300

    Add optional description for 404 page

--- src/pages/errors/404/404.tsx
+++ src/pages/errors/404/404.tsx
@@ -2,12 +2,16 @@ import { Link } from 'react-router-dom';
 import { APP_ROUTES } from '@/constants/routes';
 import styles from './styles.module.css';
 
-function Error404(): JSX.Element {
+type Error404Props = {
+  description?: string;
+}
+
+function Error404({description}: Error404Props): JSX.Element {
   return (
     <div className={`page ${styles.errorPage}`}>
       <div className={styles.errorBlock}>
         <h1 className={styles.errorCode}>404</h1>
-        <h2 className={styles.errorMessage}>Page not found</h2>
+        <h2 className={styles.errorMessage}>{description ?? 'Not found'}</h2>
       </div>
       <Link to={APP_ROUTES.MAIN} className={styles.linkMain}>
         Main page
