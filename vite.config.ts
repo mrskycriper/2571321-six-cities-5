f2510ca8bb28b80b846a6807d900630bad084bc3
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2025-01-04 00:45:53 +0300

    Fix missing semicolon

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
